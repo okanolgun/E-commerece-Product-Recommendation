42d1acb0e904b8f5acaac253ba9fafd7e8cd3885
Okan Olgun <98118524+okanolgun@users.noreply.github.com>
2024-08-25 12:54:27 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, request, render_template
-import pandas as pd
-import random
-from flask_sqlalchemy import SQLAlchemy
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 
+
+app = Flask(__name__)
+
+# routes
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+if __name__=='__main__':
+    app.run(debug=True)
